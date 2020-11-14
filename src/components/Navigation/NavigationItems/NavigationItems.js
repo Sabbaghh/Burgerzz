637487b29e0e791906723e88fc547859a95325d2
abdallah Sabbagh <afsabbagh9@gmail.com>
2020-11-14 05:12:16 +0200

    fixing the main router

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -5,7 +5,7 @@ import NavigationItem from './NavigationItem/NavigationItem'
 export const NavigationItems = (props) => {
     return (
         <ul className='NavigationItems'>
-            <NavigationItem link='/' itemStyle>
+            <NavigationItem link='/Burgerzz' itemStyle>
                 BurgerBuilder
             </NavigationItem>
             <NavigationItem link='/orders' >

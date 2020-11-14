f67ee2e8695688915c7310a0d7ae7c4be1838bad
abdallah Sabbagh <afsabbagh9@gmail.com>
2020-11-14 04:51:46 +0200

    Updates

---
+++ precache-manifest.e0c71c95f2c20818fa7032c4cb4fee9f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a712d0bbaa0f44c3ee7578f4720bb1f",
+    "url": "/Burgerzz/index.html"
+  },
+  {
+    "revision": "400f129727398a9a10bb",
+    "url": "/Burgerzz/static/css/main.fb047b8e.chunk.css"
+  },
+  {
+    "revision": "919091ab4b37bae9e0fa",
+    "url": "/Burgerzz/static/js/2.c6c3f766.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Burgerzz/static/js/2.c6c3f766.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "400f129727398a9a10bb",
+    "url": "/Burgerzz/static/js/main.dd33e01d.chunk.js"
+  },
+  {
+    "revision": "70178de803951b6baedf",
+    "url": "/Burgerzz/static/js/runtime-main.69021e15.js"
+  },
+  {
+    "revision": "d36bd09acd0710a7bab1d3fae6743998",
+    "url": "/Burgerzz/static/media/burger.d36bd09a.png"
+  }
+]);
